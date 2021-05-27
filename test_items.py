89659dcf0458915738590c7aaa17037967d76835
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-05-27 16:03:47 +0300

    start initial

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_find_btn_add_to_cart_item(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
+        'Кнопка добавления товара в корзину отсутсвует'
